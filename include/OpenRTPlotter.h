2531350a04c325e04e8dea5b109b1d8171e8e0ca
AnomyDragon <44169154+nlund18@users.noreply.github.com>
2020-04-29 13:29:07 +0200

    Implemented the PImpl structure (#22)
    closes #15

--- include/OpenRTPlotter.h
+++ include/OpenRTPlotter.h
@@ -17,8 +17,6 @@
 #include "../src/util/Util.h"
 
 namespace OpenRTP {
-    class OpenRTPlotter;
-
     struct InitStruct
     {
         std::string Title;            //Window title
@@ -49,74 +47,21 @@ namespace OpenRTP {
         }
     };
 
-    class OpenRTPlotter
+    class Plotter
     {
     public:
-        OpenRTPlotter();
-        //OpenRTPlotter(InitStruct PlotInfo, Plot SinglePlot);
-        OpenRTPlotter(InitStruct PlotInfo, std::vector<Plot> MultiPlot);
+        Plotter();
+        Plotter(InitStruct PlotInfo, std::vector<Plot> MultiPlot);
+        ~Plotter();
 
-        int OpenRTPlotterInit();
-        int OpenRTPlotterRun();
-        void InsertPointByName(std::string Name, Point SinglePoint);
-        void InsertPointRangeByName(std::string Name, std::vector<Point> SinglePoint, int Start, int End);
+        int Init();
+        int RunPlot();
         void InsertByPlot(std::vector<Plot> Plot);
 
     private:
-        int CreateWindow();
-        glm::mat4 ViewportTransform(float x, float y, float width, float height, float *pixel_x,    float *pixel_y);
-        void Draw();
-        void GraphDraw(glm::mat4 Transform, int WindowWidth, int WindowHeight);
-        void GraphLine();
-        void PointDraw(Plot ToDraw);
-        void BorderDraw(glm::mat4 Transform, int WindowWidth, int WindowHeight);
-        void CalcTicks();
-        void Free();
-        int Resources();
-        void InitData();
-        void Input();
-        void TestFunctions();
-        InitStruct Info;
-
-        GLFWwindow* window;
-        Font *mFont;
-        Util *MUtil;
-
-        float width, height; //Window size
-        float *PixelX, *PixelY; //Pixel size
-        std::vector<Plot> ToPlot; //Store all plotting data
-        //TmpValues
-        const int margin = 20;
-        const int ticksize = 20;
-        float TickX = 1.0;
-        float STickX = 0.5;
-
-        float offset_x = -10;
-        float offset_y = -10;
-        float scale = 0.1;
-        float ScaleX = 0.1;
-        float ScaleY = 0.1;
-        float XScale = 10;
-        float YScale = 10;
-        float SpeedX = 0.3;
-        float SpeedY = 0.3;
-
-        float TickspacingX = 0;
-        float TickspacingY = 0;
-        float Left = 0;
-    	float Right = 0;
-    	float Bottom = 0;
-    	float Top = 0;
-        
-        GLuint Program;
-        GLint attribute_coord2d;
-        GLint uniform_color;
-        GLint uniform_transform;
-        GLuint vbo[3];
-        GLuint Spec[2];
-        GLuint LineBuf;
-        GLuint PlotBuf;
-        bool MultiLine;
-        Font::atlas *at;
+        class Impl;
+        std::unique_ptr<Impl> PImpl;
     };
+
+    //class OpenRTPlotter;
 }
--- src/Font.cpp
+++ src/Font.cpp
@@ -5,233 +5,253 @@
 
 namespace OpenRTP
 {
-    Font::Font(GLFWwindow *TWindow)
+    class Font::Impl
     {
-        Window = TWindow;
-    }
+        GLFWwindow* Window;
+  	    GLuint Program;
+  	    GLint AttribCoord;
+  	    GLint UniformColor;
+  	    GLuint vbo;
 
-    int Font::Init(Util* MUtil)
-    {
-        /* Initialize the FreeType2 library */
-    	if (FT_Init_FreeType(&ft)) {
-    		fprintf(stderr, "Could not init freetype library\n");
-    		return 0;
-    	}
-
-    	program = MUtil->CreateProgram(FontVert, FontFrag);
-    	if(program == 0)
-    		return 0;
-
-    	attribute_coord = MUtil->GetAttrib(program, "coord");
-    	uniform_tex = MUtil->GetUniform(program, "tex");
-    	uniform_color = MUtil->GetUniform(program, "color");
-
-    	if(attribute_coord == -1 || uniform_tex == -1 || uniform_color == -1)
-    		return 0;
-
-    	// Create the vertex buffer object
-    	glGenBuffers(1, &vbo);
-
-    	return 1;
-    }
-
-    int Font::CreateAtlas(atlas *ToCreate, const char* FontFilename, int FontSize)
-    {
-        /* Load a font */
-    	if (FT_New_Face(ft, FontFilename, 0, &face)) {
-    		fprintf(stderr, "Could not open font %s\n", FontFilename);
-    		return 0;
-    	}
-
-        //ToCreate = new atlas(face, FontSize, uniform_tex);
-
-        return 1;
-    }
-
-    void Font::TextDraw(std::string text, atlas *FontAtlas, float mx, float my, RenderMode Mode)
-    {
-        int WindowWidth, WindowHeight;
-        glfwGetFramebufferSize(Window, &WindowWidth, &WindowHeight);
-
-        glUseProgram(program);
-        GLfloat black[4] = { 0, 0, 0, 1 };
-        glViewport(0, 0, WindowWidth, WindowHeight);
-
-        /* Enable blending, necessary for our alpha texture */
-	    glEnable(GL_BLEND);
-	    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-        /* Set font size to 48 pixels, color to black */
-	    glUniform4fv(uniform_color, 1, black);
-
-        glBindTexture(GL_TEXTURE_2D, FontAtlas->tex);
-        glUniform1i(uniform_tex, 0);
-
-        glEnableVertexAttribArray(attribute_coord);
-	    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	    glVertexAttribPointer(attribute_coord, 4, GL_FLOAT, GL_FALSE, 0, 0);
-        int tc = 0;
-        std::vector<TextPoint> CoordsV;
-
-        switch (Mode)
+  	    FT_Library ft;
+    public:
+        Impl(GLFWwindow* TWindow)
         {
-        case RenderRight:
-            CoordsV = TextRight(text, &tc, FontAtlas, mx, my);
-            break;
-        case RenderLeft:
-            CoordsV = TextLeft(text, &tc, FontAtlas, mx, my);
-            break;
-        }
-        //std::vector<TextPoint> CoordsV = TextLeft(text, &tc, FontAtlas, mx, my);
-
-        glViewport(0, 0, WindowWidth, WindowHeight);
-
-        glBufferData(GL_ARRAY_BUFFER, CoordsV.size() * sizeof(TextPoint), &CoordsV.front(), GL_DYNAMIC_DRAW);
-	    glDrawArrays(GL_TRIANGLES, 0, tc);
-
-	    glDisableVertexAttribArray(attribute_coord);
-    }
-
-    std::vector<TextPoint> Font::TextLeft(std::string Text, int *NumOfChar, atlas *FontAtlas, float x, float y)
-    {   
-        int WindowWidth, WindowHeight;
-        glfwGetFramebufferSize(Window, &WindowWidth, &WindowHeight);
-
-        std::vector<TextPoint> Coords;
-
-        const char *CText = Text.c_str();
-
-        float XPos, YPos;
-        float Width, Height;
-        float SizeX = 2.0 / WindowWidth;
-        float SizeY = 2.0 / WindowHeight;
-
-        for (int TPointer = 0; TPointer < Text.length(); TPointer++)
-        {
-            *NumOfChar += 6;
-            XPos = x;
-            YPos = -y;
-            Width = FontAtlas->c[Text[TPointer]].bw * SizeX;
-            Height = FontAtlas->c[Text[TPointer]].bh * SizeY;
-
-            x += FontAtlas->c[Text[TPointer]].ax * SizeX;
-            y += FontAtlas->c[Text[TPointer]].ay * SizeY;
-
-            if(!Width || !Height)
-                continue;
-
-            Coords.push_back((TextPoint) 
-            {   XPos, 
-                -YPos,
-                FontAtlas->c[Text[TPointer]].tx, 
-                FontAtlas->c[Text[TPointer]].ty
-            });
-            Coords.push_back((TextPoint) 
-            {   XPos + Width, 
-                -YPos, 
-                FontAtlas->c[Text[TPointer]].tx + FontAtlas->c[Text[TPointer]].bw /FontAtlas->w,
-                FontAtlas->c[Text[TPointer]].ty
-            });
-            Coords.push_back((TextPoint) 
-            {   XPos, 
-                -YPos - Height, 
-                FontAtlas->c[Text[TPointer]].tx,
-                FontAtlas->c[Text[TPointer]].ty + FontAtlas->c[Text[TPointer]].bh / FontAtlas->h
-            });
-            Coords.push_back((TextPoint) 
-            {   XPos + Width, 
-                -YPos, 
-                FontAtlas->c[Text[TPointer]].tx + FontAtlas->c[Text[TPointer]].bw /FontAtlas->w,
-                FontAtlas->c[Text[TPointer]].ty
-            });
-            Coords.push_back((TextPoint) 
-            {   XPos, 
-                -YPos - Height, 
-                FontAtlas->c[Text[TPointer]].tx,
-                FontAtlas->c[Text[TPointer]].ty + FontAtlas->c[Text[TPointer]].bh / FontAtlas->h
-            });
-            Coords.push_back((TextPoint) 
-            {   XPos + Width,
-                -YPos - Height, 
-                FontAtlas->c[Text[TPointer]].tx + FontAtlas->c[Text[TPointer]].bw /FontAtlas->w,
-                FontAtlas->c[Text[TPointer]].ty + FontAtlas->c[Text[TPointer]].bh / FontAtlas->h
-            });
+            Window = TWindow;
         }
 
-        return Coords;
-    }
-
-    std::vector<TextPoint> Font::TextRight(std::string Text, int *NumOfChar, atlas *FontAtlas, float x, float y)
-    {
-        int WindowWidth, WindowHeight;
-        glfwGetFramebufferSize(Window, &WindowWidth, &WindowHeight);
-
-        std::vector<TextPoint> Coords;
-
-        const char *CText = Text.c_str();
-
-        float XPos, YPos;
-        float Width, Height;
-        float SizeX = 2.0 / WindowWidth;
-        float SizeY = 2.0 / WindowHeight;
-
-        for (int TPointer = Text.length() - 1; TPointer >= 0; TPointer--)
+        int InitFont(Util* MUtil, GLint* UniformTex)
         {
-            *NumOfChar += 6;
-            XPos = x;
-            YPos = -y;
-            Width = FontAtlas->c[Text[TPointer]].bw * SizeX;
-            Height = FontAtlas->c[Text[TPointer]].bh * SizeY;
+            /* Initialize the FreeType2 library */
+    	    if (FT_Init_FreeType(&ft)) {
+    	    	fprintf(stderr, "Could not init freetype library\n");
+    	    	return 0;
+    	    }
 
-            x -= FontAtlas->c[Text[TPointer]].ax * SizeX;
-            y -= FontAtlas->c[Text[TPointer]].ay * SizeY;
+    	    Program = MUtil->CreateProgram(FontVert, FontFrag);
+    	    if(Program == 0)
+    	    	return 0;
 
-            if(!Width || !Height)
-                continue;
+    	    AttribCoord = MUtil->GetAttrib(Program, "coord");
+    	    *UniformTex = MUtil->GetUniform(Program, "tex");
+    	    UniformColor = MUtil->GetUniform(Program, "color");
 
-            Coords.push_back((TextPoint) 
-            {   XPos, 
-                -YPos,
-                FontAtlas->c[Text[TPointer]].tx, 
-                FontAtlas->c[Text[TPointer]].ty
-            });
-            Coords.push_back((TextPoint) 
-            {   XPos + Width, 
-                -YPos, 
-                FontAtlas->c[Text[TPointer]].tx + FontAtlas->c[Text[TPointer]].bw /FontAtlas->w,
-                FontAtlas->c[Text[TPointer]].ty
-            });
-            Coords.push_back((TextPoint) 
-            {   XPos, 
-                -YPos - Height, 
-                FontAtlas->c[Text[TPointer]].tx,
-                FontAtlas->c[Text[TPointer]].ty + FontAtlas->c[Text[TPointer]].bh / FontAtlas->h
-            });
-            Coords.push_back((TextPoint) 
-            {   XPos + Width, 
-                -YPos, 
-                FontAtlas->c[Text[TPointer]].tx + FontAtlas->c[Text[TPointer]].bw /FontAtlas->w,
-                FontAtlas->c[Text[TPointer]].ty
-            });
-            Coords.push_back((TextPoint) 
-            {   XPos, 
-                -YPos - Height, 
-                FontAtlas->c[Text[TPointer]].tx,
-                FontAtlas->c[Text[TPointer]].ty + FontAtlas->c[Text[TPointer]].bh / FontAtlas->h
-            });
-            Coords.push_back((TextPoint) 
-            {   XPos + Width,
-                -YPos - Height, 
-                FontAtlas->c[Text[TPointer]].tx + FontAtlas->c[Text[TPointer]].bw /FontAtlas->w,
-                FontAtlas->c[Text[TPointer]].ty + FontAtlas->c[Text[TPointer]].bh / FontAtlas->h
-            });
+    	    if(AttribCoord == -1 || *UniformTex == -1 || UniformColor == -1)
+    	    	return 0;
+
+    	    // Create the vertex buffer object
+    	    glGenBuffers(1, &vbo);
+
+    	    return 1;
         }
 
-        return Coords;
-    }
+        int CreateAtlas(atlas *ToCreate, const char* FontFilename, int FontSize, FT_Face *FontFace)
+        {
+            /* Load a font */
+    	    if (FT_New_Face(ft, FontFilename, 0, FontFace)) {
+    	    	fprintf(stderr, "Could not open font %s\n", FontFilename);
+    	    	return 0;
+    	    }
 
-    void Font::Free()
-    {
-        glDeleteProgram(program);
-    }
+            //ToCreate = new atlas(face, FontSize, uniform_tex);
+
+            return 1;
+        }
+
+        void TextDraw(std::string text, atlas *FontAtlas, float mx, float my, RenderMode Mode, GLint UniformTex)
+        {
+            int WindowWidth, WindowHeight;
+            glfwGetFramebufferSize(Window, &WindowWidth, &WindowHeight);
+
+            glUseProgram(Program);
+            GLfloat black[4] = { 0, 0, 0, 1 };
+            glViewport(0, 0, WindowWidth, WindowHeight);
+
+            /* Enable blending, necessary for our alpha texture */
+	        glEnable(GL_BLEND);
+	        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+            /* Set font size to 48 pixels, color to black */
+	        glUniform4fv(UniformColor, 1, black);
+
+            glBindTexture(GL_TEXTURE_2D, FontAtlas->tex);
+            glUniform1i(UniformTex, 0);
+
+            glEnableVertexAttribArray(AttribCoord);
+	        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	        glVertexAttribPointer(AttribCoord, 4, GL_FLOAT, GL_FALSE, 0, 0);
+            int tc = 0;
+            std::vector<TextPoint> CoordsV;
+
+            switch (Mode)
+            {
+            case RenderRight:
+                CoordsV = TextRight(text, &tc, FontAtlas, mx, my);
+                break;
+            case RenderLeft:
+                CoordsV = TextLeft(text, &tc, FontAtlas, mx, my);
+                break;
+            }
+            //std::vector<TextPoint> CoordsV = TextLeft(text, &tc, FontAtlas, mx, my);
+
+            glViewport(0, 0, WindowWidth, WindowHeight);
+
+            glBufferData(GL_ARRAY_BUFFER, CoordsV.size() * sizeof(TextPoint), &CoordsV.front(),     GL_DYNAMIC_DRAW);
+	        glDrawArrays(GL_TRIANGLES, 0, tc);
+
+	        glDisableVertexAttribArray(AttribCoord);
+        }
+
+        void Free()
+        {
+            glDeleteProgram(Program);
+        }
+
+    private:
+        std::vector<TextPoint> TextLeft(std::string Text, int *NumOfChar, atlas *FontAtlas, float x, float y)
+        {   
+            int WindowWidth, WindowHeight;
+            glfwGetFramebufferSize(Window, &WindowWidth, &WindowHeight);
+
+            std::vector<TextPoint> Coords;
+
+            const char *CText = Text.c_str();
+
+            float XPos, YPos;
+            float Width, Height;
+            float SizeX = 2.0 / WindowWidth;
+            float SizeY = 2.0 / WindowHeight;
+
+            for (int TPointer = 0; TPointer < Text.length(); TPointer++)
+            {
+                *NumOfChar += 6;
+                XPos = x;
+                YPos = -y;
+                Width = FontAtlas->c[Text[TPointer]].bw * SizeX;
+                Height = FontAtlas->c[Text[TPointer]].bh * SizeY;
+
+                x += FontAtlas->c[Text[TPointer]].ax * SizeX;
+                y += FontAtlas->c[Text[TPointer]].ay * SizeY;
+
+                if(!Width || !Height)
+                    continue;
+
+                Coords.push_back((TextPoint) 
+                {   XPos, 
+                    -YPos,
+                    FontAtlas->c[Text[TPointer]].tx, 
+                    FontAtlas->c[Text[TPointer]].ty
+                });
+                Coords.push_back((TextPoint) 
+                {   XPos + Width, 
+                    -YPos, 
+                    FontAtlas->c[Text[TPointer]].tx + FontAtlas->c[Text[TPointer]].bw /FontAtlas->w,
+                    FontAtlas->c[Text[TPointer]].ty
+                });
+                Coords.push_back((TextPoint) 
+                {   XPos, 
+                    -YPos - Height, 
+                    FontAtlas->c[Text[TPointer]].tx,
+                    FontAtlas->c[Text[TPointer]].ty + FontAtlas->c[Text[TPointer]].bh / FontAtlas->h
+                });
+                Coords.push_back((TextPoint) 
+                {   XPos + Width, 
+                    -YPos, 
+                    FontAtlas->c[Text[TPointer]].tx + FontAtlas->c[Text[TPointer]].bw /FontAtlas->w,
+                    FontAtlas->c[Text[TPointer]].ty
+                });
+                Coords.push_back((TextPoint) 
+                {   XPos, 
+                    -YPos - Height, 
+                    FontAtlas->c[Text[TPointer]].tx,
+                    FontAtlas->c[Text[TPointer]].ty + FontAtlas->c[Text[TPointer]].bh / FontAtlas->h
+                });
+                Coords.push_back((TextPoint) 
+                {   XPos + Width,
+                    -YPos - Height, 
+                    FontAtlas->c[Text[TPointer]].tx + FontAtlas->c[Text[TPointer]].bw /FontAtlas->w,
+                    FontAtlas->c[Text[TPointer]].ty + FontAtlas->c[Text[TPointer]].bh / FontAtlas->h
+                });
+            }
+
+            return Coords;
+        }
+
+        std::vector<TextPoint> TextRight(std::string Text, int *NumOfChar, atlas *FontAtlas, float x, float y)
+        {
+            int WindowWidth, WindowHeight;
+            glfwGetFramebufferSize(Window, &WindowWidth, &WindowHeight);
+
+            std::vector<TextPoint> Coords;
+
+            const char *CText = Text.c_str();
+
+            float XPos, YPos;
+            float Width, Height;
+            float SizeX = 2.0 / WindowWidth;
+            float SizeY = 2.0 / WindowHeight;
+
+            for (int TPointer = Text.length() - 1; TPointer >= 0; TPointer--)
+            {
+                *NumOfChar += 6;
+                XPos = x;
+                YPos = -y;
+                Width = FontAtlas->c[Text[TPointer]].bw * SizeX;
+                Height = FontAtlas->c[Text[TPointer]].bh * SizeY;
+
+                x -= FontAtlas->c[Text[TPointer]].ax * SizeX;
+                y -= FontAtlas->c[Text[TPointer]].ay * SizeY;
+
+                if(!Width || !Height)
+                    continue;
+
+                Coords.push_back((TextPoint) 
+                {   XPos, 
+                    -YPos,
+                    FontAtlas->c[Text[TPointer]].tx, 
+                    FontAtlas->c[Text[TPointer]].ty
+                });
+                Coords.push_back((TextPoint) 
+                {   XPos + Width, 
+                    -YPos, 
+                    FontAtlas->c[Text[TPointer]].tx + FontAtlas->c[Text[TPointer]].bw /FontAtlas->w,
+                    FontAtlas->c[Text[TPointer]].ty
+                });
+                Coords.push_back((TextPoint) 
+                {   XPos, 
+                    -YPos - Height, 
+                    FontAtlas->c[Text[TPointer]].tx,
+                    FontAtlas->c[Text[TPointer]].ty + FontAtlas->c[Text[TPointer]].bh / FontAtlas->h
+                });
+                Coords.push_back((TextPoint) 
+                {   XPos + Width, 
+                    -YPos, 
+                    FontAtlas->c[Text[TPointer]].tx + FontAtlas->c[Text[TPointer]].bw /FontAtlas->w,
+                    FontAtlas->c[Text[TPointer]].ty
+                });
+                Coords.push_back((TextPoint) 
+                {   XPos, 
+                    -YPos - Height, 
+                    FontAtlas->c[Text[TPointer]].tx,
+                    FontAtlas->c[Text[TPointer]].ty + FontAtlas->c[Text[TPointer]].bh / FontAtlas->h
+                });
+                Coords.push_back((TextPoint) 
+                {   XPos + Width,
+                    -YPos - Height, 
+                    FontAtlas->c[Text[TPointer]].tx + FontAtlas->c[Text[TPointer]].bw /FontAtlas->w,
+                    FontAtlas->c[Text[TPointer]].ty + FontAtlas->c[Text[TPointer]].bh / FontAtlas->h
+                });
+            }
+
+            return Coords;
+        }
+    };
+    
+    Font::Font(GLFWwindow* TWindow) : PImpl{std::make_unique<Impl>(TWindow)} {}
+    Font::~Font() = default;
+
+    int Font::InitFont(Util* MUtil) { return PImpl->InitFont(MUtil, &UniformTex); }
+    int Font::CreateAtlas(atlas *ToCreate, const char* FontFilename, int FontSize) { return PImpl->CreateAtlas(ToCreate, FontFilename, FontSize, &Face); }
+    void Font::TextDraw(std::string text, atlas *FontAtlas, float mx, float my, RenderMode Mode) { PImpl->TextDraw(text, FontAtlas, mx, my, Mode, UniformTex); }
+	void Font::Free() { PImpl->Free(); }
 }

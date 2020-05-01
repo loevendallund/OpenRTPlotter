771889943247427b3fc8c68c378324f6f6d9e54e
Rasmus Rendal <rasmus@rend.al>
2020-05-01 10:56:43 +0200

    Updated nix derivation to use unstable glfw (#26)

--- default.nix
+++ default.nix
@@ -1,10 +1,15 @@
 with import <nixpkgs> {};
 
+let
+  unstable = import
+    (fetchTarball
+      https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz) {};
+in
 stdenv.mkDerivation {
   name = "chemilang";
   src = ./.;
 
-  buildInputs = [ cmake clang gtest glew glfw freetype glm ];
+  buildInputs = [ cmake clang gtest glew unstable.glfw freetype glm ];
 
   buildPhase = "cmake . && make";
 
